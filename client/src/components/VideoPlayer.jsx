1998c9ef307ba19ab10b6fd7590318373f0ed902
Abhinandan Jha <92037313+Avi-J0506@users.noreply.github.com>
2023-08-11 12:52:59 +0530

    App complted

---
+++ client/src/components/VideoPlayer.jsx
@@ -0,0 +1,66 @@
+import React, { useContext } from 'react'
+import { Grid, Typography, Paper } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
+
+import { SocketContext } from '../SocketContext';
+
+const useStyles = makeStyles((theme) => ({
+  video: {
+    width: '550px',
+    [theme.breakpoints.down('xs')]: {
+      width: '300px',
+    },
+  },
+  gridContainer: {
+    justifyContent: 'center',
+    [theme.breakpoints.down('xs')]: {
+      flexDirection: 'column',
+    },
+  },
+  paper: {
+    padding: '10px',
+    border: '2px solid black',
+    margin: '10px',
+  },
+}));
+
+const VideoPlayer = () => {
+  const { name, callAccepted, myVideo, userVideo, callEnded, stream, call } = useContext(SocketContext)
+  const classes = useStyles();
+  return (
+    <div>
+      <Grid container className='{classes.gridContainer'>
+
+        {/*Our  Video*/}
+        {
+          stream && (
+        <Paper className={classes.paper}>
+          <Grid item xs={12} md={6}>
+              <Typography variant='h5' gutterBottom>
+              {name || 'Name'}              
+              </Typography>
+              <video playsInline muted ref={myVideo} className={classes.video} autoPlay/>
+          </Grid>
+        </Paper>
+          )
+        }
+
+        {/*Users  Video*/}
+        {
+        callAccepted && !callEnded &&(
+          <Paper className={classes.paper}>
+            <Grid item xs={12} md={6}>
+                <Typography variant='h5' gutterBottom>
+                {call.name || 'Name'}
+                </Typography>
+                <video playsInline ref={userVideo} className={classes.video} autoPlay/>
+            </Grid>
+          </Paper>
+           )
+        }
+      </Grid>
+    </div>
+  )
+}
+
+export default VideoPlayer
